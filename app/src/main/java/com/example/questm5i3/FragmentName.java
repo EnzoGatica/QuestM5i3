2ea3c8edb9f66f6f6cd57d7d1f5aecac9f6856ae
Enzo_Gatica <enzogatica96@gmail.com>
2023-07-05 20:51:49 -0400

    Diseño listo, navs

--- app/src/main/java/com/example/questm5i3/FragmentName.java
+++ app/src/main/java/com/example/questm5i3/FragmentName.java
@@ -8,12 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.questm5i3.databinding.FragmentNameBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FragmentName#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class FragmentName extends Fragment {
+
+    private FragmentNameBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +62,11 @@ public class FragmentName extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        binding = FragmentNameBinding.inflate(getLayoutInflater());
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_name, container, false);
+        return binding.getRoot();
     }
 }
